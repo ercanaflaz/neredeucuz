12ec065b410ac33fac72e7902e09c5c0e6d29aeb
Ercan <aflazercan@gmail.com>
2026-07-22 17:14:57 +0300

    giris: facebook kaldirildi, google + e-posta

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { LogOut, Mail } from 'lucide-react'
-import { kayitOl, girisYap, cikisYap, googleIleGiris, facebookIleGiris } from '../lib/auth'
+import { kayitOl, girisYap, cikisYap, googleIleGiris } from '../lib/auth'
 
 export default function Login({ user }) {
   const [mod, setMod] = useState('giris') // giris | kayit
@@ -10,13 +10,12 @@ export default function Login({ user }) {
   const [bilgi, setBilgi] = useState(null)
   const [bekle, setBekle] = useState(false)
 
-  async function sosyal(saglayici) {
+  async function googleGiris() {
     setHata(null)
     try {
-      if (saglayici === 'google') await googleIleGiris()
-      else await facebookIleGiris()
+      await googleIleGiris()
     } catch (err) {
-      setHata(cevir(err?.message) + ' (Sağlayıcı Supabase\'de açık değilse önce oradan etkinleştir.)')
+      setHata(cevir(err?.message))
     }
   }
 
@@ -61,13 +60,9 @@ export default function Login({ user }) {
 
       {/* Sosyal giriş */}
       <div className="space-y-2">
-        <button type="button" onClick={() => sosyal('google')} className="btn btn-outline w-full gap-2">
+        <button type="button" onClick={googleGiris} className="btn btn-outline w-full gap-2">
           <svg width="18" height="18" viewBox="0 0 48 48"><path fill="#FFC107" d="M43.6 20.5H42V20H24v8h11.3C33.7 32.4 29.3 35 24 35c-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.9 1.2 8 3.1l5.7-5.7C34.6 5.1 29.6 3 24 3 12.4 3 3 12.4 3 24s9.4 21 21 21 21-9.4 21-21c0-1.4-.1-2.4-.4-3.5z"/><path fill="#FF3D00" d="M6.3 14.7l6.6 4.8C14.7 16 19 13 24 13c3.1 0 5.9 1.2 8 3.1l5.7-5.7C34.6 5.1 29.6 3 24 3 16 3 9.1 7.6 6.3 14.7z"/><path fill="#4CAF50" d="M24 45c5.2 0 9.9-2 13.4-5.2l-6.2-5.2C29.2 35.9 26.7 37 24 37c-5.3 0-9.7-2.6-11.3-6.9l-6.5 5C9.1 42.3 16 45 24 45z"/><path fill="#1976D2" d="M43.6 20.5H42V20H24v8h11.3c-.8 2.2-2.2 4.1-4.1 5.6l6.2 5.2C40.9 36 45 30.6 45 24c0-1.4-.1-2.4-.4-3.5z"/></svg>
           Google ile devam et
-        </button>
-        <button type="button" onClick={() => sosyal('facebook')} className="btn btn-outline w-full gap-2">
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="#1877F2"><path d="M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.05V9.41c0-3.02 1.79-4.68 4.53-4.68 1.31 0 2.68.24 2.68.24v2.97h-1.51c-1.49 0-1.96.93-1.96 1.89v2.24h3.33l-.53 3.49h-2.8V24C19.61 23.1 24 18.1 24 12.07z"/></svg>
-          Facebook ile devam et
         </button>
       </div>
 
